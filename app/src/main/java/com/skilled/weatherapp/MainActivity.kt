c09d79a2f3ae3739117acac2c7dd369208f85922
skillelis <fgfayzullaev@gmail.com>
2021-10-06 03:25:55 +0300

    commit main activity_main.xml

--- app/src/main/java/com/skilled/weatherapp/MainActivity.kt
+++ app/src/main/java/com/skilled/weatherapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.skilled.weatherapp
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.appbar.AppBarLayout
+import com.skilled.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        collapseTitleBar()
+    }
+
+    private fun collapseTitleBar() {
+        var isShow = true
+        var scrollRange = -1
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { barLayout, verticalOffset ->
+            if (scrollRange == -1) {
+                scrollRange = barLayout?.totalScrollRange!!
+            }
+            if (scrollRange + verticalOffset == 0) {
+                binding.collapsingToolbar.title = "Rostov-on-Don"
+                isShow = true
+                binding.collapseToolBarConstLayot.visibility = View.INVISIBLE
+            } else if (isShow) {
+                binding.collapsingToolbar.title = " "//careful there should a space between double quote otherwise it wont work
+                binding.collapseToolBarConstLayot.visibility = View.VISIBLE
+                isShow = false
+            }
+        })
     }
 }
--- app/src/main/java/com/skilled/weatherapp/SplashActivity.kt
+++ app/src/main/java/com/skilled/weatherapp/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.skilled.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashActivity: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        splashActivity = findViewById(R.id.splash_activity)
+        splashActivity.alpha = 0f
+        splashActivity.animate().setDuration(2000).alpha(1f).withEndAction {
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
